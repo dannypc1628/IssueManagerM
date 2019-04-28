c1f3ae3a53882d8a47eb0368707157e90a319c89
dannyLiu <h152074716@gmail.com>
2019-04-28 19:02:59 +0800

    完成初步登入與註冊功能

--- IssueManagerM/Controllers/LoginController.cs
+++ IssueManagerM/Controllers/LoginController.cs
@@ -10,6 +10,7 @@ namespace IssueManagerM.Controllers
 {
     public class LoginController : Controller
     {
+        private IssueManagerEntities db = new IssueManagerEntities();
         // GET: Login
         public ActionResult Index()
         {
@@ -21,7 +22,13 @@ namespace IssueManagerM.Controllers
         {
             if (ModelState.IsValid)
             {
-                return Content("正確");
+                var data = from user in db.User
+                           where user.UserID == LoginData.UserID && user.Password == LoginData.Password
+                           select user;
+                if (data.Count() == 1)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
             }
             return View();
         }
@@ -36,7 +43,27 @@ namespace IssueManagerM.Controllers
         {
             if (ModelState.IsValid)
             {
-                return Content("正確");
+                var data = from user in db.User
+                           where user.UserID == SignUpData.UserID
+                           select user.UserID;
+                if (data.Count() == 0)
+                {
+                    db.User.Add(new User()
+                    {
+                        UserID = SignUpData.UserID,
+                        UserName = SignUpData.Name,
+                        Password = SignUpData.Password,
+                        Phone = SignUpData.Phone,
+                        Email = SignUpData.Email
+                    });
+                    db.SaveChanges();
+
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    ModelState.AddModelError("UserID", "該帳號已註冊！");
+                }
             }
             return View();
         }
--- IssueManagerM/Models/ViewModels/SignupViewModel.cs
+++ IssueManagerM/Models/ViewModels/SignupViewModel.cs
@@ -9,27 +9,35 @@ namespace IssueManagerM.Models.ViewModels
     public class SignUpViewModel
     {
         [Required(ErrorMessage ="必須輸入身分證字號")]
+        [StringLength(10,ErrorMessage ="身份證字號應該是10個字元",MinimumLength =10)]
+        [RegularExpression(@"^[A-Z]\d{9}$",ErrorMessage ="開頭為大寫英文字元一個後面接9個數字")]
         [Display(Name="身份證字號")]
         public string UserID { get; set; }
 
         [Required(ErrorMessage = "姓名不能空白")]
         [Display(Name = "姓名")]
+        [StringLength(10, ErrorMessage = "姓名應該是2-10個字元", MinimumLength = 2)]
         public string Name { get; set; }
 
         [Required(ErrorMessage ="密碼不能空白")]
         [Display(Name="密碼")]
+        [StringLength(10, ErrorMessage = "密碼長度需要是6-10個字元", MinimumLength = 6)]
         public string Password { get; set; }
 
         [Required(ErrorMessage = "密碼不能空白")]
         [Display(Name ="再輸入一次密碼")]
+        [Compare("Password",ErrorMessage ="兩次密碼不相同")]
         public string SecondPassword { get; set; }
 
         [Required(ErrorMessage = "電話不能空白")]
         [Display(Name="電話")]
+        [RegularExpression(@"^\d{8,10}$", ErrorMessage = "電話只能8到10位數之數字")]
         public string Phone { get; set; }
 
         [Required(ErrorMessage = "電子信箱不能空白")]
         [Display(Name="電子信箱")]
+        [EmailAddress(ErrorMessage ="這不是E-mail格式")]
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
     }
 }
